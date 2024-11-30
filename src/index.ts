d0a9b3d38f2388e1d73a42bc53d0bada385c8bd5
Kaarel <kaareloun@gmail.com>
2024-11-30 05:49:05 +0200

    refactor: extract fns

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ import { execa } from 'execa'
 import fs from 'fs/promises'
 
 async function main() {
-  const [, , ...args] = process.argv
   const usesHusky = await fs
     .stat('.husky')
     .then(() => true)
     .catch(() => false)
-  const verbose = args.includes('--verbose') || usesHusky
 
+  const [, , ...args] = process.argv
+  const verbose = args.includes('--verbose') || usesHusky
   const noBreaking = args.includes('--no-breaking')
   const noTask = args.includes('--no-task')
   const noWip = args.includes('--no-wip')
@@ -24,21 +24,32 @@ async function main() {
     process.exit()
   }
 
-  const handleStep = async (
-    handlerPromise: Promise<string | boolean | symbol>,
-  ) => {
-    const res = await handlerPromise
-    if (isCancel(res)) {
-      outro('Cancelled')
-      process.exit()
-    }
-
-    return res
-  }
-
   intro(`Committing ${files} staged file${files > 1 ? 's' : ''}`)
 
-  let commitType = await handleStep(
+  let commitType = await promptCommitType()
+  const breakingChange = noBreaking ? false : await promptBreakingChange()
+  const message = await promptMessage()
+  const taskNumber = noTask ? undefined : await promptTaskNumber()
+  const isWIP = noWip ? false : await promptIsWIP()
+
+  if (breakingChange) {
+    commitType = `${commitType}!`
+  }
+
+  const fullMessage = `${commitType.toString()}: ${message.toString()}${isWIP ? ' (WIP)' : ''}${taskNumber ? ` (${taskNumber.toString()})` : ''}`
+
+  await execa({
+    stdout: verbose ? process.stdout : 'ignore',
+    stderr: verbose ? process.stdout : 'ignore',
+  })`git commit -m ${fullMessage}`
+  outro(`Committed "${fullMessage}"`)
+
+  const { stdout: gitStatus } = await execa`git status`
+  console.log(gitStatus)
+}
+
+async function promptCommitType() {
+  return await handleStep(
     select({
       message: 'Select the type of change you are commiting',
       options: [
@@ -60,20 +71,16 @@ async function main() {
       initialValue: 'feat',
     }),
   )
-
-  const breakingChange = noBreaking
-    ? false
-    : await handleStep(
-        text({
-          message: `Does this change introduce a breaking change? (press ${chalk.cyan('[enter]')} to skip)`,
-        }),
-      )
-
-  if (breakingChange) {
-    commitType = `${commitType}!`
-  }
-
-  const message = await handleStep(
+}
+async function promptBreakingChange() {
+  return await handleStep(
+    text({
+      message: `Does this change introduce a breaking change? (press ${chalk.cyan('[enter]')} to skip)`,
+    }),
+  )
+}
+async function promptMessage() {
+  return await handleStep(
     text({
       message:
         'Write a short & imperative summary of the code changes (lower case and no period)',
@@ -82,31 +89,28 @@ async function main() {
       },
     }),
   ).then((msg) => `${msg}`.toLowerCase().trim())
+}
+async function promptTaskNumber() {
+  return await handleStep(
+    text({
+      message: `Provide task number, (press ${chalk.cyan('[enter]')} to skip)`,
+    }),
+  )
+}
+async function promptIsWIP() {
+  return await handleStep(
+    confirm({ message: 'Is this task a WIP?', initialValue: false }),
+  )
+}
 
-  const taskNumber = noTask
-    ? undefined
-    : await handleStep(
-        text({
-          message: `Provide task number, (press ${chalk.cyan('[enter]')} to skip)`,
-        }),
-      )
+async function handleStep(handlerPromise: Promise<string | boolean | symbol>) {
+  const res = await handlerPromise
+  if (isCancel(res)) {
+    outro('Cancelled')
+    process.exit()
+  }
 
-  const isWIP = noWip
-    ? false
-    : await handleStep(
-        confirm({ message: 'Is this task a WIP?', initialValue: false }),
-      )
-
-  const fullMessage = `${commitType.toString()}: ${message.toString()}${isWIP ? ' (WIP)' : ''}${taskNumber ? ` (${taskNumber.toString()})` : ''}`
-
-  await execa({
-    stdout: verbose ? process.stdout : 'ignore',
-    stderr: verbose ? process.stdout : 'ignore',
-  })`git commit -m ${fullMessage}`
-  outro(`Committed "${fullMessage}"`)
-
-  const { stdout: gitStatus } = await execa`git status`
-  console.log(gitStatus)
+  return res
 }
 
 main().catch((e) => {
